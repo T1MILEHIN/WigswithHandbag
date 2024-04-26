71e6699a18317af745be09d491fea70d4e6890a5
Timi <tunmisebabade@gmail.com>
2024-04-26 13:43:20 +0100

    first commit

--- src/contexts/cartContext.jsx
+++ src/contexts/cartContext.jsx
@@ -5,19 +5,10 @@ import { Toaster, toast } from 'sonner';
 export const CartContext = createContext({})
 
 export const CartProvider = ({children}) => {
-    const [cartItem, setCartItem] = useState(() => {
-        if (localStorage.getItem("CartItem")) {
-            return JSON.parse(localStorage.getItem("CartItem")) || [];
-        }
-    });
+    const [cartItem, setCartItem] = useState([])
     const addToCart = (data) => {
         toast.success(`Successfully added to cart`);
     }
-
-    useEffect(() => {
-        const items = JSON.parse(localStorage.getItem("CartItem")) || [];
-        setCartItem(items);
-    }, [])
 
     return (
         <CartContext.Provider value={{cartItem, addToCart}}>
