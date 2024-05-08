a0a3643cd93a7af346bfcd2fba7fc88e175219b1
mayowasamuel <fpasamuelmayowa51@gmail.com>
2024-05-08 16:02:05 +0100

    new commit line

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <div className="">
         {/* <Header /> */}
-        <Landing />
+        {/* <Landing /> */}
       </div>
       <Footer />
     </>
